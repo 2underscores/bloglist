4980660d42625d90169affbdba189d06dade1d44
2underscores <jeremy__smith@hotmail.com>
2024-10-16 19:46:37 +1100

    MongoDB connection works

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -12,8 +13,13 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const { MONGO_USER: USER, MONGO_PASSWORD: PASS, MONGO_CLUSTER: CLUSTER, MONGO_TABLE: TABLE } = process.env
+const mongoUrl = `mongodb+srv://${USER}:${PASS}@${CLUSTER}.ljiec.mongodb.net/${TABLE}?retryWrites=true&w=majority&appName=${CLUSTER}`
+
+console.log('connecting to MongoDB at ', mongoUrl)
 mongoose.connect(mongoUrl)
+  .then(() => { console.log(`connected to MongoDB cluster "${CLUSTER}", table "${TABLE}", as user "${USER}"`) })
+  .catch(error => { console.log('error connecting to MongoDB:', error.message) })
 
 app.use(cors())
 app.use(express.json())
@@ -22,6 +28,7 @@ app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
+      console.log(`Retrieved ${blogs.length} blogs`)
       response.json(blogs)
     })
 })
@@ -36,7 +43,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
