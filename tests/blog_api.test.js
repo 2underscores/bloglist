7166afbc12b5a1d0e9bf11bd63d3d6b019fd7eb2
2underscores <jeremy__smith@hotmail.com>
2024-10-18 08:53:18 +1100

    VSCode debugger launch config to run tests in debug mode

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,20 +1,25 @@
 const { test, describe, after } = require('node:test')
+const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app')
-const mongoose = require('mongoose') // App initialises mongo, but doesn't close it. Test close it.
+const mongoose = require('mongoose') // App initializes mongo, but doesn't close it. Test close it.
 
 const api = supertest(app)
 
 // TODO: Switch to a testing DB!
 describe('CRUD on documents', (() => {
   test('Notes returned', async () => {
-    await api
+    const resp = await api
       .get('/api/blogs')
       .expect(200)
       .expect('content-type', /application\/json/)
+    assert(resp.body.length > 10)
   })
 }))
 
 after(async () => {
   await mongoose.connection.close()
+  // If connection left open, process never end.
+  // Can see in debugger the top "blog_api.test.js" process never ends so the test execution is never done.
+  // stack is: test:watch: npm -> cross-env -> blog_api.test.js
 })
