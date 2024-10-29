e052c8af289a34ccde56a56d7097c61540b5b845
2underscores <jeremy__smith@hotmail.com>
2024-10-30 07:26:43 +1100

    Fixed devmode env imports

--- utils/config.js
+++ utils/config.js
@@ -11,6 +11,6 @@ module.exports = {
     CLUSTER: process.env.MONGO_CLUSTER,
     TABLE: ENV === 'test' ? process.env.MONGO_TABLE_TEST : process.env.MONGO_TABLE, //Will be a more re-usable pattern. TODO: don't default prod
   },
-  SALT_ROUNDS: ENV === 'test' ? 1 : process.env.SALT_ROUNDS,
+  SALT_ROUNDS: ENV === 'test' ? 1 : Number(process.env.SALT_ROUNDS),
   AUTH_SECRET: process.env.AUTH_SECRET
 }
